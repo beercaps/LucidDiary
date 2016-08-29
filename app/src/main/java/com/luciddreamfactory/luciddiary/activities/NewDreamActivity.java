d6e43d2194f6e5f07772c47df7f55c7a4f321387
Kevin <janwetzelspam@gmail.com>
2016-08-29 15:25:15 +0200

    added color picker added recipient edit text view

--- app/src/main/java/com/luciddreamfactory/luciddiary/activities/NewDreamActivity.java
+++ app/src/main/java/com/luciddreamfactory/luciddiary/activities/NewDreamActivity.java
@@ -12,9 +12,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
+import com.android.colorpicker.ColorPickerDialog;
+import com.android.colorpicker.ColorPickerSwatch;
+import com.android.ex.chips.BaseRecipientAdapter;
+import com.android.ex.chips.RecipientEditTextView;
 import com.luciddreamfactory.luciddiary.R;
 import com.luciddreamfactory.luciddiary.model.MyDreamCard;
 
@@ -25,14 +34,19 @@ public class NewDreamActivity extends AppCompatActivity implements View.OnClickL
     private static final String TAG = NewDreamActivity.class.getSimpleName();
 
     private EditText datePicker;
+    private ImageButton colorPicker;
 
+    private ScrollView newDreamScrollView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         setContentView(R.layout.activity_new_dream);
+        newDreamScrollView = (ScrollView) findViewById(R.id.new_dream_scroll_view);
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
 
         LayoutInflater inflater = getLayoutInflater();
@@ -49,6 +63,19 @@ public class NewDreamActivity extends AppCompatActivity implements View.OnClickL
         datePicker = (EditText) findViewById(R.id.datePicker);
         datePicker.setText(R.string.last_night);
 
+        colorPicker = (ImageButton) findViewById(R.id.bt_color_picker);
+        colorPicker.setOnClickListener(this);
+
+
+
+        RecipientEditTextView myRetv =
+                (RecipientEditTextView) findViewById(R.id.ret_tags);
+
+        myRetv.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+        myRetv.setAdapter(new BaseRecipientAdapter(this));
+
+
 
     }
 
@@ -61,6 +88,13 @@ public class NewDreamActivity extends AppCompatActivity implements View.OnClickL
                 break;
             case R.id.action_menu_save:
                 Log.d(TAG, "onClick: save");  break;
+
+            case R.id.bt_color_picker:
+                Log.d(TAG, "onClick: color Picker");
+                showColorPicker();
+
+
+
         }
     }
 
@@ -70,9 +104,47 @@ public class NewDreamActivity extends AppCompatActivity implements View.OnClickL
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
+    private void showColorPicker(){
+        int[] colorsArray = createColorArray();
+        ColorPickerDialog colorPickerDialog = new ColorPickerDialog();
+        colorPickerDialog.initialize(R.string.color_title, colorsArray,0, 5,colorsArray.length );
+        colorPickerDialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+            @Override
+            public void onColorSelected(int color) {
+                Log.d(TAG, "onColorSelected: "+color);
+                newDreamScrollView.setBackgroundColor(color);
+            }
+        });
+        colorPickerDialog.show(getFragmentManager(), "colorPicker");
+    }
 
+private int[] createColorArray(){
+    int[] colors = {
+            getColor(R.color.red),
+            getColor(R.color.pink),
+            getColor(R.color.purple),
+            getColor(R.color.deep_purple),
+            getColor(R.color.indigo),
+            getColor(R.color.blue),
+            getColor(R.color.light_blue),
+            getColor(R.color.cyan),
+            getColor(R.color.teal),
+            getColor( R.color.green),
+            getColor(R.color.light_green),
+            getColor(R.color.lime),
+            getColor(R.color.yellow),
+            getColor(R.color.amber),
+            getColor(R.color.orange),
+            getColor(R.color.deep_orange),
+            getColor(R.color.brown),
+            getColor(R.color.grey),
+            getColor(R.color.blue_grey),
+            getColor(R.color.white)
+    };
 
+    return colors;
 
+}
 
 
 
